9c9a970f084abe1c0bebcdd9a799d980d0260af8
seok0616 <seok0616@naver.com>
2023-11-21 11:47:06 +0900

    QR 코드생성 완료

--- 4. QR코드 생성기/main4-1.py
+++ 4. QR코드 생성기/main4-1.py
@@ -1,7 +1,21 @@
 import qrcode
 
-qr_data = 'www.naver.com'
-qr_img = qrcode.make(qr_data)
+class QrMaker:
+    def __init__(self):
+        self.domain = ''
+    def set_domain(self,domain):
+        self.domain = domain
+    def save_qrcode(self,title):
+        qr_data = self.domain
+        qr_img = qrcode.make(qr_data)
 
-save_path = '4. QR코드 생성기\\' + qr_data + '.png'
-qr_img.save(save_path)
+
+        save_path = f'./{title}.png'
+        qr_img.save(save_path)
+
+if __name__ == '__main__':
+    q = QrMaker()
+    domain = input('도메인 입력')
+    q.set_domain(domain)
+    title = input('파일명 입력')
+    q.save_qrcode(title)
