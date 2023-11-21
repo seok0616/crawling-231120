54fcad5c39a9c582b2987f598ff62f5de8a11295
seok0616 <seok0616@naver.com>
2023-11-21 10:50:53 +0900

    음성전환 작업중

--- 3. 텍스트를 음성으로 변환/main3-1.py
+++ 3. 텍스트를 음성으로 변환/main3-1.py
@@ -1,6 +1,14 @@
 from gtts import gTTS
 
-text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
+class TextToSpeech:
 
-tts = gTTS(text=text, lang='ko')
-tts.save(r"./hi.mp3")
+    def __init__(self):
+        self.text = ''
+    def set_text(self,text):
+        self.text = text  # 값이 다르기 때문에 동기화 하려는 것임. (연관성 無)
+    def save_text(self):
+        tts = gTTS(text=self.text, lang='ko')
+        tts.save(r"./text.mp3")
+
+if __name__ == '__main__':
+    pass
