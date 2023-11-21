5aae4058bcdaae3d6efbcacd94208f7bb53f9a3d
seok0616 <seok0616@naver.com>
2023-11-21 15:19:28 +0900

    구글 번역

--- 7. 환율 변환기/main7-1.py
+++ 7. 환율 변환기/main7-1.py
@@ -1,4 +1,6 @@
 from currency_converter import CurrencyConverter
+import requests
+from bs4 import BeautifulSoup
 # pip install CurrencyConverter
 
 class Exchange:
@@ -13,10 +15,23 @@ class Exchange:
         cc = CurrencyConverter('http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip')
         return cc.convert(1,'USD','KRW')
 
+    def realtime_usd_to_krw(self,target1, target2):
+        headers = {
+            'User-Agent': 'Mozilla/5.0',
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+        # https://kr.investing.com/currencies/usd-krw
+        response = requests.get(f"https://kr.investing.com/currencies/{target1}-{target2}", headers=headers)
+        content = BeautifulSoup(response.content, 'html.parser')
+        containers = content.find(name = 'span',attrs = {'data-test': 'instrument-price-last'})
+        for 1 in ls:
+
+        # return containers.text
+
 if __name__ == '__main__':
     e = Exchange()
     while 1 :
-        menu = input('0-종료, 1-전체화폐단위, 2-달러를 원화로')
+        menu = input('0-종료, 1-전체화폐단위, 2-달러를 원화로, 3-실시간 환율변환')
         if menu == '0':
             print('프로그램 종료')
             break                   # 종료일 때는 break
@@ -26,6 +41,13 @@ if __name__ == '__main__':
         elif menu == '2':
             c = e.change_usd_to_krw_currencies()
             print(f'변환된 결과 : {c}')
+        elif menu == '3':
+            target1 = input('바꾸려는 화폐단위:')
+            target2 = input('바뀌는 화폐단위:')
+            c = e.realtime_usd_to_krw('USD','KRW')
+            print(f'변환된 결과 : {c}')
+
+
         else :
             print('메뉴에 없는 번호 입니다.')
             continue               # 다시 보여 줄 때 continue
--- 9. 영어로된 문서를 한글로 자동번역/main9-1.py
+++ 9. 영어로된 문서를 한글로 자동번역/main9-1.py
@@ -1,11 +1,38 @@
 import googletrans 
+# pip install googletrans==4.0.0-rc1
 
-translator = googletrans.Translator() 
+class Translater:
 
-str1 = "행복하세요" 
-result1 = translator.translate(str1, dest='en', src='auto') 
-print(f"행복하세요 => {result1.text}") 
+    def __init__(self):
+        self.translator = googletrans.Translator()
+        self.text = ''
+    def set_text(self,text):
+        self.text = text
+    def en_to_kr(self):
+        result2 = self.translator.translate(self.text, dest='ko', src='en')
+        return result2
+        # print(f"I am happy => {result2.text}")
+    def kr_to_en(self):
+        result1 = self.translator.translate(self.text, dest='en', src='auto')
+        return result1
+        # print(f"행복하세요 => {result1.text}")
 
-str2 = "I am happy" 
-result2 = translator.translate(str2, dest='ko', src='en') 
-print(f"I am happy => {result2.text}")
+if __name__ == '__main__':
+    t = Translater()
+    while 1:
+        menu = input('0-종료, 1-입력하세요, 2-영어에서 한글, 3-한글에서 영어')
+        if menu == '0':
+            print('프로그램 종료')
+            break  # 종료일 때는 break
+        elif menu == '1':
+            text = input('문장을 입력')  # 하나
+            t.set_text(text)
+        elif menu == '2':
+            a = t.en_to_kr()
+            print(f'결과는 {a}')
+        elif menu == '3':
+            a = t.kr_to_en()
+            print(f'결과는 {a}')
+        else :
+            print('메뉴에 없는 번호 입니다.')
+            continue               # 다시 보여 줄 때 continue
