74ed0bb176a54bfac1faaee9f4936ac4aba6cd1f
seok0616 <seok0616@naver.com>
2023-11-21 11:09:04 +0900

    playground install message

--- 3. 텍스트를 음성으로 변환/main3-1.py
+++ 3. 텍스트를 음성으로 변환/main3-1.py
@@ -6,9 +6,16 @@ class TextToSpeech:
         self.text = ''
     def set_text(self,text):
         self.text = text  # 값이 다르기 때문에 동기화 하려는 것임. (연관성 無)
-    def save_text(self):
+    def save_mp3(self,title):
         tts = gTTS(text=self.text, lang='ko')
-        tts.save(r"./text.mp3")
+        tts.save(f"./{title}.mp3")
 
 if __name__ == '__main__':
-    pass
+
+    t = TextToSpeech
+    title = input('제목을 입력하시오')
+    text = input('변환하려는 문장을 입력하시오')
+    t.set_text(text)
+    t.save_mp3(title)
+
+    playsound("./hi.mp3")
