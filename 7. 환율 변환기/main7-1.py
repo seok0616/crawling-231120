5496bbca6609a1fa809f416ddd40339d98d76402
seok0616 <seok0616@naver.com>
2023-11-21 13:57:37 +0900

    환율변환기작업중

--- 7. 환율 변환기/main7-1.py
+++ 7. 환율 변환기/main7-1.py
@@ -1,4 +1,31 @@
 from currency_converter import CurrencyConverter
+# pip install CurrencyConverter
 
-cc = CurrencyConverter()
-print(cc.currencies)
+class Exchange:
+    def __init__(self):
+        pass
+
+    def get_all_currencies(self):
+        cc = CurrencyConverter()
+        return cc.currencies
+
+    def change_usd_to_krw_currencies(self):
+        cc = CurrencyConverter('http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip')
+        return cc.convert(1,'USD','KRW')
+
+if __name__ == '__main__':
+    e = Exchange()
+    while 1 :
+        menu = input('0-종료, 1-전체화폐단위, 2-달러를 원화로')
+        if menu == '0':
+            print('프로그램 종료')
+            break                   # 종료일 때는 break
+        elif menu == '1':
+            c = e.get_all_currencies()
+            print(f'전체화폐 : {c}')
+        elif menu == '2':
+            c = e.change_usd_to_krw_currencies()
+            print(f'변환된 결과 : {c}')
+        else :
+            print('메뉴에 없는 번호 입니다.')
+            continue               # 다시 보여 줄 때 continue
